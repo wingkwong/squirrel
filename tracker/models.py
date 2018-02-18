9be650d045c2bc7d9d8d44cb00a094a6dceaef31
wingkwong <wingkwong.code@gmail.com>
2018-02-18 20:59:37 +0800

    fixed typo

--- tracker/models.py
+++ tracker/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Expense(models.Model):
     currency = models.CharField(max_length=3)
-    description = models.CharField(mxa_length=1000, null=True)
+    description = models.CharField(max_length=1000, null=True)
     type = models.CharField(max_length=30)
     payment = models.CharField(max_length=30)
     amount = models.IntegerField()
