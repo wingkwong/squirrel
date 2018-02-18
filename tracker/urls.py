7bf4887f46546f44c8f4c47fb1608f85ac13e6af
wingkwong <wingkwong.code@gmail.com>
2018-02-18 21:15:49 +0800

    created view & url for detail page

--- tracker/urls.py
+++ tracker/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /tracker
     path('', views.index, name ="index"),
+
+    # /tracker/{id}
+    path('<int:expense_id>', views.detail, name="detail")
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Hello World</h1>")
+
+
+def detail(request, expense_id):
+    return HttpResponse("<h2>Detail page for id: " + str(expense_id) + "</h2>")
