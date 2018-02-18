070a71f51756e8fd3769b35a205efc0af8d0a221
wingkwong <wingkwong.code@gmail.com>
2018-02-18 20:43:59 +0800

    created model class for tracker

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Expense(models.Model):
+    currency = models.CharField(max_length=3)
+    description = models.CharField(mxa_length=1000, null=True)
+    type = models.CharField(max_length=30)
+    payment = models.CharField(max_length=30)
+    amount = models.IntegerField()
+    date = models.DateField()
