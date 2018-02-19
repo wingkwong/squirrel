3a780013b01802447949c6d6b70e5a05207fd9f9
wingkwong <wingkwong.code@gmail.com>
2018-02-19 19:41:13 +0800

    used root

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,18 +4,18 @@ from . import views
 app_name = 'tracker'
 
 urlpatterns = [
-    # /tracker
+    # /
     path('', views.IndexView.as_view(), name ="index"),
 
-    # /tracker/{id}
-    path('<int:pk>', views.DetailView.as_view(), name="detail"),
-
-    # /tracker/add
+    # /add
     path('add', views.ExpenseCreate.as_view(), name="create_expense"),
 
-    # /tracker/update/{id}
+    # /update/{id}
     path('update/<int:pk>', views.ExpenseUpdate.as_view(), name="update_expense"),
 
-    # /tracker/delete/{id}
+    # /delete/{id}
     path('delete/<int:pk>', views.ExpenseDelete.as_view(), name="delete_expense"),
+
+    # /analysis
+    path('analysis', views.AnalysisView.as_view(), name="analysis")
 ]
