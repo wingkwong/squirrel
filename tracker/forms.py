68c27534b1031daa13336bc273a91755accb4ef0
wingkwong <wingkwong.code@gmail.com>
2018-02-19 18:46:01 +0800

    updated ExpenseTableHelper fields

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,17 +1,22 @@
 from django import forms
-from .models import Expense
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
+from crispy_forms.layout import Layout, Field, ButtonHolder, Submit
 
 
 class ExpenseTableHelper(FormHelper):
+    currency = forms.CharField()
+    date = forms.DateField(
+        widget=forms.TextInput(
+            attrs={'type': 'date'}
+        )
+    )
     form_method = 'GET'
     layout = Layout(
-        'currency',
-        'description',
-        'type',
-        'payment',
-        'amount',
-        'date',
-        Submit('submit', 'Filtrar'),
+        Field('currency'),
+        Field('description'),
+        Field('type'),
+        Field('payment'),
+        Field('amount'),
+        Field('date', placeholder='YYYY-MM-DD'),
+        Submit('submit', 'Filter'),
     )
