f072bbd5fdf32f3c75c98803c3ae3e13914cb7a7
wingkwong <wingkwong.code@gmail.com>
2018-02-19 18:50:40 +0800

    applied icontains to filters

--- tracker/filters.py
+++ tracker/filters.py
@@ -3,6 +3,13 @@ from .models import Expense
 
 
 class ExpenseFilter(django_filters.FilterSet):
+    currency = django_filters.CharFilter(lookup_expr='icontains')
+    description = django_filters.CharFilter(lookup_expr='icontains')
+    type = django_filters.CharFilter(lookup_expr='icontains')
+    payment = django_filters.CharFilter(lookup_expr='icontains')
+    amount = django_filters.CharFilter(lookup_expr='icontains')
+
+
     class Meta:
         model = Expense
         fields = (
