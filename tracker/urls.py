912923a9f366a01167108fdbb56f94282b1d444d
wingkwong <wingkwong.code@gmail.com>
2018-02-19 12:12:45 +0800

    added /add, /update and /delete urls

--- tracker/urls.py
+++ tracker/urls.py
@@ -8,5 +8,14 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name ="index"),
 
     # /tracker/{id}
-    path('<int:expense_id>', views.DetailView.as_view(), name="detail")
+    path('<int:pk>', views.DetailView.as_view(), name="detail"),
+
+    # /tracker/add
+    path('add', views.ExpenseCreate.as_view(), name="create_expense"),
+
+    # /tracker/update/{id}
+        path('update/<int:pk>', views.ExpenseUpdate.as_view(), name="update_expense"),
+
+    # /tracker/delete/{id}
+        path('delete/<int:pk>', views.ExpenseDelete.as_view(), name="delete_expense"),
 ]
