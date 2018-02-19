027f23f736fc70496d88692eaf93b30ff6e37a71
wingkwong <wingkwong.code@gmail.com>
2018-02-19 18:25:08 +0800

    added main.js

---
+++ tracker/static/main.js
@@ -0,0 +1,7 @@
+function openFilterPanel() {
+    document.getElementById("filterPanel").style.width = "100%";
+}
+
+function closeFilterPanel() {
+    document.getElementById("filterPanel").style.width = "0";
+}
