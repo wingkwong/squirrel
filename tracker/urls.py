68a188653af698c9003f84ec8f8d26cda21f2846
wingkwong <wingkwong.code@gmail.com>
2018-02-21 00:15:52 +0800

    update urls for analysis view

--- tracker/urls.py
+++ tracker/urls.py
@@ -19,6 +19,15 @@ urlpatterns = [
     # /analysis
     path('analysis', views.AnalysisView.as_view(), name="analysis"),
 
+    # /analysis/2018
+    path('analysis/<int:year>', views.AnalysisView.annually, name="annually"),
+
+    # /analysis/2018/02
+    path('analysis/<int:year>/<int:month>', views.AnalysisView.monthly, name="monthly"),
+
+    # /analysis/2018/02/20
+    path('analysis/<int:year>/<int:month>/<int:day>', views.AnalysisView.daily, name="daily"),
+
     # /analysis
     path('api/expense/', views.AnalysisView.get_expense_data, name="api-expense")
 ]
