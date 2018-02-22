0ebb8d913284786ea01b6b7675c0f4ff022cfd3f
wingkwong <wingkwong.code@gmail.com>
2018-02-22 22:00:44 +0800

    worked on annually report integrated with chartjs

--- tracker/views.py
+++ tracker/views.py
@@ -4,11 +4,14 @@ from django.urls import reverse_lazy
 from django_tables2 import RequestConfig
 from django.http import JsonResponse
 from django.core import serializers
+from django.shortcuts import render
+from django.db.models import Sum
 from .tables import ExpenseTable
 from .models import Expense
 from .forms import ExpenseTableHelper
 from .filters import ExpenseFilter
-from django.shortcuts import render
+import random
+
 
 class IndexView(generic.ListView):
     template_name = "tracker/index.html"
@@ -74,9 +77,64 @@ class AnalysisView(generic.ListView):
         return JsonResponse(data)
 
     def annually(request, year):
+        # retrieve distinct types
+        expense_type = list(Expense.objects.filter(date__year=year).values('type').distinct().order_by()
+                                 .values_list('type', flat=True))
+
+        datasets = []
+        months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+                  "November", "December"]
+
+        for type in expense_type:
+            # expense
+            arr = list(Expense.objects.filter(date__year=year).filter(type=type).distinct().order_by('date'))
+
+            # expense month by type
+            month_tmp_arr = list(Expense.objects.filter(date__year=year).filter(type=type).values('date').distinct()
+                            .order_by('date')
+                            .values_list('date', flat=True))
+
+            # expense amount by type
+            expense_tmp_arr = list(Expense.objects.filter(date__year=year).filter(type=type)
+                .values('date').distinct().order_by('date')
+                .annotate(amount=Sum('amount')).values_list('amount', flat=True))
+
+            # init array with size 12 with value 0
+            monthly_expense_cnt = [0] * 12
+
+            for i, m in enumerate(arr):
+                monthly_expense_cnt[m.date.month-1] += m.amount
+
+            total_amount = []
+
+            for j, k in enumerate(monthly_expense_cnt):
+                o = {}
+                o['x'] = months[j];
+                o['y'] = monthly_expense_cnt[j];
+                print(o)
+                total_amount.append(o)
+
+            # generate random color
+            r = lambda: random.randint(0, 255)
+            color = '#%02X%02X%02X' % (r(), r(), r())
+
+            # construct dataset
+            dataset = {
+                'label': type,
+                'backgroundColor': color,
+                'borderColor': color,
+                'data': total_amount,
+                'fill': 'false'
+            }
+
+            datasets.append(dataset)
+
         context = {
-            'annually_expense': Expense.objects.filter(date__year=year)
+            'datasets': datasets,
+            'months': months,
+            'title': 'Annual Report in ' + str(year)
         }
+
         return render(request, "analysis/index.html", context)
 
     def monthly(request, year, month):
