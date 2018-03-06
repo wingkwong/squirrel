4a283b95c589571dae99ae9576f310d5270621db
wingkwong <wingkwong.code@gmail.com>
2018-03-06 23:41:19 +0800

    added register view

---
+++ accounts/views.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.profile.dob = form.cleaned_data.get('dob')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('tracker:expense')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+
+def profile(request):
+    print("TODO")
