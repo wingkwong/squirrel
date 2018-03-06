6c54d41e6b8f33329ec0df05ffe4a384c6670aa6
wingkwong <wingkwong.code@gmail.com>
2018-03-06 23:40:44 +0800

    reverted urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import url
-from . import views
+from account import views
 
 urlpatterns = [
     url(r'^register/$', views.register, name='register'),
-    # url(r'^login/$', views.login, name='login'),
-    # url(r'^logout/$', views.logout, name='logout'),
     url(r'^profile/$', views.profile, name='profile')
 ]
