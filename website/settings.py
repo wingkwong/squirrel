e59f1a366ca499bac357e0c1e6a0d34e5e0bcc1d
wingkwong <wingkwong.code@gmail.com>
2018-04-13 14:08:31 +0800

    added social_django

--- website/settings.py
+++ website/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django_filters',
     'bootstrap3',
     'crispy_forms',
-    'tracker.templatetags.month_labels'
+    'tracker.templatetags.month_labels',
+    'social_django'
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -127,4 +130,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+LOGIN_URL = '/accounts/login'
 LOGIN_REDIRECT_URL = '/tracker'
+
+AUTHENTICATION_BACKENDS = (
+ 'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
+ 'social_core.backends.google.GoogleOpenId',  # for Google authentication
+ 'social_core.backends.google.GoogleOAuth2',  # for Google authentication
+ 'django.contrib.auth.backends.ModelBackend',
+)
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY =''  # Client Key
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '' # Secret Key
