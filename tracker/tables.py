b917fd13c1ebbe7c7d28d14a53eff33303bc12ee
wingkwong <wingkwong.code@gmail.com>
2018-04-20 17:05:09 +0800

    set as responsive

--- tracker/tables.py
+++ tracker/tables.py
@@ -24,5 +24,5 @@ class ExpenseTable(tables.Table):
             )
         attrs = {"class": "table table-striped dt-responsive nowrap"}
         empty_text = "No records found"
-        template_name = 'django_tables2/bootstrap.html'
+        template_name = 'django_tables2/bootstrap-responsive.html'
 
