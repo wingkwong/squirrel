14a3d68a32ba6b8e2619d90b5ac24070fb3e08f8
wingkwong <wingkwong.code@gmail.com>
2018-04-25 00:28:02 +0800

    fixed #11

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from accounts import views
 
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('profile/', views.ProfileView.profile, name='profile')
+    path('profile/', login_required(views.ProfileView.profile), name='profile')
 ]
